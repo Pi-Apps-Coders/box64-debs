e0586d14a94be1d8a48412d25b1758b3dfa295ff
theofficialgman <28281419+theofficialgman@users.noreply.github.com>
2023-08-01 14:46:27 -0400

    Revert "specify `CMAKE_SYSTEM_PROCESSOR` for cross compilation"
    This reverts commit 54999d71274dd33eccb186259e2ad7784019c698.

--- create-deb.sh
+++ create-deb.sh
@@ -47,10 +47,12 @@ for target in ${targets[@]}; do
 
   cd $DIRECTORY/box64
   sudo rm -rf build && mkdir build && cd build || error "Could not move to build directory"
+  # allow installation even on x86_64 (needed for checkinstall)
+  sed -i "s/NOT _x86 AND NOT _x86_64/true/g" ../CMakeLists.txt
   if [[ $target == "ANDROID" ]]; then
-    cmake .. -DBAD_SIGNAL=ON -DCMAKE_BUILD_TYPE=RelWithDebInfo -DCMAKE_C_COMPILER=aarch64-linux-gnu-gcc-8 -DARM_DYNAREC=ON -DCMAKE_SYSTEM_PROCESSOR=aarch64 || error "Failed to run cmake."
+    cmake .. -DBAD_SIGNAL=ON -DCMAKE_BUILD_TYPE=RelWithDebInfo -DCMAKE_C_COMPILER=aarch64-linux-gnu-gcc-8 -DARM_DYNAREC=ON || error "Failed to run cmake."
   else
-    cmake .. -D$target=1 -DCMAKE_BUILD_TYPE=RelWithDebInfo -DCMAKE_C_COMPILER=aarch64-linux-gnu-gcc-8 -DARM_DYNAREC=ON -DCMAKE_SYSTEM_PROCESSOR=aarch64 || error "Failed to run cmake."
+    cmake .. -D$target=1 -DCMAKE_BUILD_TYPE=RelWithDebInfo -DCMAKE_C_COMPILER=aarch64-linux-gnu-gcc-8 -DARM_DYNAREC=ON || error "Failed to run cmake."
   fi
   make -j4 || error "Failed to run make."
 
