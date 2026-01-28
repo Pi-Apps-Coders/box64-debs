71796277243368a81b6686a8f6a56675669385d2
matu6968 <wierzejskimateusz8@gmail.com>
2026-01-28 23:44:14 +0100

    add RPI5ARM64 target... after 2 years since the Pi 5 released

--- create-deb.sh
+++ create-deb.sh
@@ -41,7 +41,7 @@ echo "box64 is not the latest version, compiling now."
 echo $commit > $DIRECTORY/commit.txt
 echo "Wrote commit to commit.txt file for use during the next compilation."
 
-targets=(GENERIC_ARM ANDROID RPI4ARM64 RPI3ARM64 TEGRAX1 RK3399)
+targets=(GENERIC_ARM ANDROID RPI5ARM64 RPI4ARM64 RPI3ARM64 TEGRAX1 RK3399)
 
 for target in ${targets[@]}; do
 
