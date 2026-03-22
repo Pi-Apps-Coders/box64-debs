5637d296b3b3fd8d73a4f343734c9c57ea0ddbe9
theofficialgman <28281419+theofficialgman@users.noreply.github.com>
2026-03-22 15:27:15 -0400

    test gcc ppa

--- .github/workflows/install-dependencies-and-build.sh
+++ .github/workflows/install-dependencies-and-build.sh
@@ -4,13 +4,13 @@ apt update
 apt install -y software-properties-common lsb-release \
 sudo wget curl build-essential jq autoconf automake \
 pkg-config ca-certificates rpm apt-utils \
-python3 make gettext pinentry-tty devscripts dpkg-dev \
-gcc-13 g++-13
+python3 make gettext pinentry-tty devscripts dpkg-dev
 # install new enough git to run actions/checkout
 sudo add-apt-repository ppa:git-core/ppa -y
 sudo add-apt-repository ppa:theofficialgman/cmake-bionic -y
+sudo add-apt-repository ppa:ubuntu-toolchain-r/test -y
 sudo apt update
-sudo apt install -y git cmake
+sudo apt install -y git cmake gcc-13 g++-13
 # avoid "fatal: detected dubious ownership in repository" error
 git config --global --add safe.directory '*'
 
